26edf7a1135b01eb082e0a9cdbf1c4213a464984
or334 <39033230+or334@users.noreply.github.com>
2020-01-28 12:21:33 +0200

    End of section 7

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -3,12 +3,21 @@ import { HomeComponent } from './Home/Home.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
+import { AuthService } from './_services/auth.service';
 
 // tslint:disable-next-line: one-variable-per-declaration
 export const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'members', component: MemberListComponent },
-  { path: 'messages', component: MessagesComponent },
-  { path: 'lists', component: ListsComponent },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  { path: '', component: HomeComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthService],
+    children: [
+      { path: 'members', component: MemberListComponent,},
+      { path: 'messages', component: MessagesComponent },
+      { path: 'lists', component: ListsComponent }
+    ]
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
